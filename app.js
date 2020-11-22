141e63102484e5667a211bdc8f1d5aa6da4a949c
Judith Dea <judithdea@gmail.com>
2020-11-21 22:02:35 -0500

    updated image galerie to keep HTML DRY and make adding images easier

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
 $(document).on("click", ".navbar-collapse.show", function (e) {
   $(this).collapse("hide");
 });
+
+
+let i = 0; // start point for slideshow
+let numberOfImages = 34; // that's simply the number of haus images in the folder
+let images = [];
+let time = 5000; // time until image switches
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+// create images list
+let j;
+for (j = 1; j <= numberOfImages; j++){
+    images.push("http://datkrummehuus.de/img/haus"+j+".jpg");
+}
+
+document.slide.src = images[i];
+
+prevBtn.addEventListener("click", () => {
+        if (i > 0){
+            i--;
+            document.slide.src = images[i];
+        } else {
+            i = numberOfImages-1;
+            document.slide.src = images[i];
+        }     
+});
+
+nextBtn.addEventListener("click", () => {
+    if (i < numberOfImages-1){
+        i++,
+        document.slide.src = images[i]
+    } else {
+        i = 0;
+        document.slide.src = images[i]
+    }
+});
+
+
+// change image
+function changeImg(){
+    document.slide.src = images[i];
+    if(i < images.length -1){
+        i++; 
+    } else {
+        i = 0; // to make it start over once it went through all images
+    }
+    setTimeout("changeImg()", time)
+}
+
+// to start slider when page loads
+window.onload = changeImg;
